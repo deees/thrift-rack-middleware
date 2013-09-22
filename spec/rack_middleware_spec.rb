d5bda6fe5026caed3a56e340d5dfafeaed2fbfb8
Alexander Kotliarskyi <alex.frantic@gmail.com>
2013-09-22 18:35:13 +0100

    Fixed RSpec deprecation warning

--- spec/rack_middleware_spec.rb
+++ spec/rack_middleware_spec.rb
@@ -24,11 +24,13 @@ require 'thrift/rack_middleware'
 
   describe RackMiddleware do
     before(:each) do
-      @processor = mock("Processor")
-      @factory = mock("ProtocolFactory")
-      @mock_app = mock("AnotherRackApp")
+      @processor = double("Processor")
+      @factory = double("ProtocolFactory")
+      @mock_app = double("AnotherRackApp")
       @middleware = RackMiddleware.new(@mock_app, :processor => @processor, :protocol_factory => @factory)
     end
+
+    let(:request_body) { StringIO.new 'test' }
 
     it "should call next rack application in the stack if request was not a post and not pointed to the hook_path" do
       env = {"REQUEST_METHOD" => "GET", "PATH_INFO" => "not_the_hook_path"}
@@ -38,11 +40,11 @@ require 'thrift/rack_middleware'
     end
 
     it "should serve using application/x-thrift" do
-      env = {"REQUEST_METHOD" => "POST", "PATH_INFO" => "/rpc_api"}
-      IOStreamTransport.stub!(:new)
-      @factory.stub!(:get_protocol)
-      @processor.stub!(:process)
-      response = mock("RackResponse")
+      env = {"REQUEST_METHOD" => "POST", "PATH_INFO" => "/rpc_api", "rack.input" => request_body}
+      IOStreamTransport.stub(:new)
+      @factory.stub(:get_protocol)
+      @processor.stub(:process)
+      response = double("RackResponse")
       response.should_receive(:[]=).with("Content-Type", "application/x-thrift")
       response.should_receive(:finish)
       Rack::Response.should_receive(:new).and_return(response)
@@ -50,34 +52,33 @@ require 'thrift/rack_middleware'
     end
 
     it "should use the IOStreamTransport" do
-      body = mock("body")
-      env = {"REQUEST_METHOD" => "POST", "PATH_INFO" => "/rpc_api", "rack.input" => body}
-      output = mock("output")
+      env = {"REQUEST_METHOD" => "POST", "PATH_INFO" => "/rpc_api", "rack.input" => request_body}
+      output = double("output")
       output.should_receive(:rewind)
       StringIO.should_receive(:new).and_return(output)
-      protocol = mock("protocol")
-      transport = mock("transport")
-      IOStreamTransport.should_receive(:new).with(body, output).and_return(transport)
+      protocol = double("protocol")
+      transport = double("transport")
+      IOStreamTransport.should_receive(:new).with(request_body, output).and_return(transport)
       @factory.should_receive(:get_protocol).with(transport).and_return(protocol)
       @processor.should_receive(:process).with(protocol, protocol)
-      response = mock("RackResponse")
-      response.stub!(:[]=)
+      response = double("RackResponse")
+      response.stub(:[]=)
       response.should_receive(:finish)
       Rack::Response.should_receive(:new).and_return(response)
       @middleware.call(env)
     end
 
     it "should have appropriate defaults for hook_path and protocol_factory" do
-      mock_factory = mock("BinaryProtocolFactory")
-      mock_proc = mock("Processor")
+      mock_factory = double("BinaryProtocolFactory")
+      mock_proc = double("Processor")
       BinaryProtocolFactory.should_receive(:new).and_return(mock_factory)
       rack_middleware = RackMiddleware.new(@mock_app, :processor => mock_proc)
       rack_middleware.hook_path.should == "/rpc_api"
     end
 
     it "should understand :hook_path, :processor and :protocol_factory" do
-      mock_proc = mock("Processor")
-      mock_factory = mock("ProtocolFactory")
+      mock_proc = double("Processor")
+      mock_factory = double("ProtocolFactory")
 
       rack_middleware = RackMiddleware.new(@mock_app, :processor => mock_proc,
                                                       :protocol_factory => mock_factory,
